57cb4f3c6cc3951dc07f1c775f0b0d13ff5e3039
Noam Musba <noammu@wix.com>
2024-06-10 16:50:01 +0300

    asdfas

---
+++ src/_codux/boards/anotherrrrrrrrr/anotherrrrrrrrr.board.tsx
@@ -0,0 +1,8 @@
+import { createBoard } from '@wixc3/react-board';
+import { Anotherrrrrrrrr } from '../../../components/anotherrrrrrrrr/anotherrrrrrrrr';
+
+export default createBoard({
+    name: 'Anotherrrrrrrrr',
+    Board: () => <Anotherrrrrrrrr />,
+    isSnippet: true,
+});
